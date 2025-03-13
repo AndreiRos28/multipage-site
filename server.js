1c8cd1fefd3497785503d2c688059bccc8dc2269
AndreiRos28 <mamrosales01@tip.edu.ph>
2025-03-13 19:16:04 +0800

    Added dynamic blog page

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// Serve about and contact pages
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// Serve blog page
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
+});
+
+// API route to fetch blog posts
+app.get('/api/posts', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Failed to load blog posts' });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
 });
 
 app.listen(PORT, () => {
